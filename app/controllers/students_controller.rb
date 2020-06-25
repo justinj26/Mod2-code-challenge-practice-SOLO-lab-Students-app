5cc0a6bab53b4e52c93f93176c3a16d5831fcd95
justin_james <jjames11799@gmail.com>
2020-06-25 12:59:40 -0400

    done

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -20,11 +20,11 @@ class StudentsController < ApplicationController
     @student = Student.new(student_params)
       if @student.valid?
         @student.save
+        redirect_to instructor_path(@instructor)
       else
         flash[:errors] = @student.errors.full_messages
         redirect_to new_student_path
       end
-    redirect_to instructor_path(@instructor)
   end
 
   def edit 
@@ -32,14 +32,14 @@ class StudentsController < ApplicationController
   end
 
   def update
-    @student.atrributes = student_params 
+    @student.attributes = student_params 
     if @student.valid? 
       @student.save
+      redirect_to student_path(@student)
     else
       flash[:errors] = @student.errors.full_messages 
-      redirect_to new_student_path 
+      redirect_to edit_student_path(@student) 
     end
-    redirect_to student_path(@student)
   end
 
 
